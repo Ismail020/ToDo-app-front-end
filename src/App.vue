3128fc1a457d2a4cafe4ce660366a23f541865bc
Ismail020 <isocetin1907@gmail.com>
2023-06-09 14:41:55 +0200

    Template

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
+import Navbar from '@/components/Navbar.vue'
+import TasksPage from '@/pages/TasksPage.vue'
+import Footer from '@/components/Footer.vue'
 </script>
 
 <template>
-  <h1>
-    Hello World
-  </h1>
+  <Navbar />
+  <TasksPage />
+  <Footer />
 </template>
 
 <style scoped>
