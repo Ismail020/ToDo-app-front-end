e27f0b8919d7298a0c8d20b9619a45f18b6f5a98
Ismail020 <isocetin1907@gmail.com>
2023-06-11 15:16:34 +0200

    Implemented the api calls

---
+++ src/components/tasks/TaskActions.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="task-actions">
+    <button class="btn btn-sm btn-circle btn-outline-secondary me-1" @click="emit('edit')">
+      <IconPencil />
+    </button>
+    <button class="btn btn-sm btn-circle btn-outline-danger" @click="emit('delete')">
+      <IconTrash />
+    </button>
+  </div>
+</template>
+
+<script setup>
+import IconPencil from '@/components/icons/IconPencil.vue'
+import IconTrash from '@/components/icons/IconTrash.vue'
+
+const emit = defineEmits(['edit, delete'])
+</script>
---
+++ src/components/tasks/Tasks.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card mt-2" v-if="tasks.length && show">
+    <ul class="list-group list-group-flush">
+      <Task v-for="task in tasks" 
+            :key="task.id" 
+            :task="task" 
+            @updated="$emit('updated', $event)"
+            @completed="$emit('completed', $event)"
+            @deleted="$emit('deleted', $event)"
+      />
+    </ul> 
+  </div>
+</template>
+
+<script setup>
+import Task from '@/components/tasks/Task.vue'
+
+defineProps({
+  tasks: {
+    type: Array,
+    required: true
+  },
+  show: {
+    type: Boolean,
+    default: true
+  }
+})
+</script>
