5851a96e1fb8e5c104a6f9f962696b74a71769e3
Ismail020 <isocetin1907@gmail.com>
2023-06-16 14:56:23 +0200

    Vue router

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <main style="min-height: 50vh; margin-top: 2rem;">
+    <h1>Login</h1>
+  </main>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter;
+</script>
